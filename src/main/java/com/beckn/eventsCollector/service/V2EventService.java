bff48f6e7a0753f3cd555eff5d2052143d3ce2fa
mayoor-em <mayoor.dabade@eminds.ai>
2022-12-28 20:38:10 +0530

    Added api for GetEventCodeDetails and exception handling

--- src/main/java/com/beckn/eventsCollector/service/V2EventService.java
+++ src/main/java/com/beckn/eventsCollector/service/V2EventService.java
@@ -3,11 +3,15 @@ package com.beckn.eventsCollector.service;
 import com.beckn.eventsCollector.dto.V2EventDTO;
 import com.beckn.eventsCollector.exception.EventException;
 import com.beckn.eventsCollector.mapper.V2EventDtoToEventMapper;
+import com.beckn.eventsCollector.model.EventMessage;
 import com.beckn.eventsCollector.model.V2Event;
+import com.beckn.eventsCollector.model.V2EventMessage;
 import com.beckn.eventsCollector.model.V2Experience;
+import com.beckn.eventsCollector.repository.V2EventMessageRepository;
 import com.beckn.eventsCollector.repository.V2EventRepository;
 import com.beckn.eventsCollector.repository.V2ExperienceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,6 +27,9 @@ public class V2EventService {
     private V2ExperienceRepository experienceRepository;
 
     @Autowired
+    private V2EventMessageRepository eventMessageRepository;
+
+    @Autowired
     private SequenceGeneratorService service;
 
     @Autowired
@@ -36,10 +43,18 @@ public class V2EventService {
 
     public String getLatestExperienceSession() {
         List<V2Experience> experiences = experienceRepository.findByActiveFlag(true);
-        if(experiences.isEmpty())
-        {
-            throw new EventException("", "", "E-101", "No active experience data found in database.");
+        if (experiences.isEmpty()) {
+            throw new EventException("Application error", HttpStatus.NOT_FOUND.toString(), "/event/experience", "Latest experience session could not be retrieved.");
         }
         return experiences.get(0).getExperience_id();
+    }
+
+    public EventMessage GetEventCodeDetails(String eventCode) {
+        V2EventMessage v2EventMessage = eventMessageRepository.findByCode(eventCode);
+        if (v2EventMessage == null) {
+            throw new EventException("Application error", HttpStatus.NOT_FOUND.toString(), "/event/code/" + eventCode, "Event code details could not be retrieved.");
+        }
+        return new EventMessage(v2EventMessage.getCode(), v2EventMessage.getAction_message(),
+                v2EventMessage.getBap_message(), v2EventMessage.getBpp_message());
     }
 }
